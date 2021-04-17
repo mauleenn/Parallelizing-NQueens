1f912a0c3df597e31fe9f82b1db8e68cb33338e5
mauleenn <mauleen.ndlovu@outlook.com>
2021-04-17 17:54:46 -0500

    Changed board size for the sake of testing timing

--- Parallel/n_queens.cpp
+++ Parallel/n_queens.cpp
@@ -1,3 +1,6 @@
+// Parallel version of the N-Queens problem.
+
+
 #include <iostream>  
 #include <omp.h>
 #include <time.h>
@@ -10,81 +13,81 @@ double startTime, endTime;
 int numofSol = 0;
 
 // Board size and number of queens
-#define N 16
+#define N 11
 
 void placeQ(int queens[], int row, int column) {
     
     for(int i = 0; i < row; i++) {
-
-   // Vertical
-   if (queens[i] == column) {
-       return;
-   }
-
-   // Two queens in the same diagonal
-    if (abs(queens[i] - column) == (row-  i))  {
-       return;
-    }
-} // End of placeQ
-
-// Set the queen
-queens[row] = column;
-
-if(row == N-1) {
-
-    #pragma omp atomic 
-    {
-        numofSol++;  //Placed final queen, found a solution
-    }
-
-    #pragma omp critical
-    {
-        std::cout << "The number of solutions found is: " << numofSol << std::endl; 
-        for (int row = 0; row < N; row++) {
-            for (int column = 0; column < N; column++) {
-                if (queens[row] == column) {
-                    std::cout << "X";
-                    }
-                else {
-                    std::cout << "|";
-                }
-            }
-        std::cout  << "\n"  << std::endl; 
+        // Vertical
+        if (queens[i] == column) {
+            return;
+        }
+        
+        // Two queens in the same diagonal
+        if (abs(queens[i] - column) == (row-  i))  {
+            return;
         }
     }
-}
-
-else {
-     // Increment row
-     for(int i = 0; i < N; i++) {
-         placeQ(queens, row + 1, i);
-     }
-}
+    
+    // Set the queen
+    queens[row] = column;
+    
+    if(row == N-1) {
+        
+        #pragma omp atomic 
+            numofSol++;  //Placed the final queen, found a solution
+        
+        #pragma omp critical
+        {
+            std::cout << "The number of solutions found is: " << numofSol << std::endl; 
+            for (int row = 0; row < N; row++) {
+                for (int column = 0; column < N; column++) {
+                    if (queens[row] == column) {
+                        std::cout << "X";
+                    }
+                    else {
+                        std::cout << "|";
+                    }
+                }
+                std::cout  << "\n"  << std::endl; 
+            }
+        }
+    }
+    
+    else {
+        
+        // Increment row
+        for(int i = 0; i < N; i++) {
+            placeQ(queens, row + 1, i);
+        }
+    }
 } // End of placeQ()
 
 void solve() {
-    #pragma omp parallel
+    #pragma omp parallel num_threads(30)
     #pragma omp single
-  {
-      for(int i = 0; i < N; i++) {
-          #pragma omp task
-          {
-              placeQ(new int[N], 0, i);
-          }
+    {
+        for(int i = 0; i < N; i++) {
+            // New task added for first row and each column recursion.
+            #pragma omp task
+            { 
+                placeQ(new int[N], 0, i);
+            }
         }
-  }
+    }
 } // end of solve()
 
 int main(int argc, char*argv[]) {
 
-   startTime = omp_get_wtime();   
-   solve();
-   endTime = omp_get_wtime();
+    startTime = omp_get_wtime();   
+    solve();
+    endTime = omp_get_wtime();
   
- // Print board size, number of solutions, and execution time 
- std::cout << "Board Size: " << N << std::endl; 
- std::cout << "Number of solutions: " << numofSol << std::endl; 
- std::cout << "Execution time: " << endTime - startTime << " seconds." << std::endl; 
-
-return 0;
+    // Print board size, number of solutions, and execution time. 
+    std::cout << "Board Size: " << N << std::endl; 
+    std::cout << "Number of solutions: " << numofSol << std::endl; 
+    std::cout << "Execution time: " << endTime - startTime << " seconds." << std::endl; 
+    
+    return 0;
 }
+
--- Parallel/n_queens_sequential.cpp
+++ Parallel/n_queens_sequential.cpp
@@ -9,29 +9,25 @@ double startTime, endTime;
 int numofSol = 0;
 
 // Board size and number of queens
-#define N 8
+#define N 14
 
 void placeQ(int queens[], int row, int column) {
     
     for(int i = 0; i < row; i++) {
-
-   // Vertical
-   if (queens[i] == column) {
-       return;
-   }
-
-   // Two queens in the same diagonal
-    if (abs(queens[i] - column) == (row-  i))  {
-       return;
+        // Vertical
+        if (queens[i] == column) {
+            return;
+        }
+        // Two queens in the same diagonal
+         if (abs(queens[i] - column) == (row-  i))  {
+             return;
+             }
     }
-} // End of placeQ
-
-// Set the queen
-queens[row] = column;
-
-if(row == N-1) {
+    // Set the queen
+    queens[row] = column;
+    if(row == N-1) {
         numofSol++;  //Placed final queen, found a solution
-
+        
         std::cout << "The number of solutions found is: " << numofSol << std::endl; 
         for (int row = 0; row < N; row++) {
             for (int column = 0; column < N; column++) {
@@ -44,33 +40,33 @@ if(row == N-1) {
             }
         std::cout  << "\n"  << std::endl; 
         }
-}
-
-else {
-     // Increment row
-     for(int i = 0; i < N; i++) {
-         placeQ(queens, row + 1, i);
-     }
-}
+    }
+    
+    else {
+        // Increment row
+        for(int i = 0; i < N; i++) {
+            placeQ(queens, row + 1, i);
+        }
+    }
 } // End of placeQ()
 
 void solve() {
-      for(int i = 0; i < N; i++) {
-              placeQ(new int[N], 0, i);
-        }
+    for(int i = 0; i < N; i++) {
+        placeQ(new int[N], 0, i);
+    }
 } // end of solve()
 
 int main(int argc, char*argv[]) {
 
-   auto begin = std::chrono::high_resolution_clock::now(); 
-   solve();
-   auto end = std::chrono::high_resolution_clock::now();
-   auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-  
- // Print board size, number of solutions, and execution time 
- std::cout << "Board Size: " << N << std::endl; 
- std::cout << "Number of solutions: " << numofSol << std::endl; 
- std::cout << "Execution time: " << elapsed.count() * 1e-9 << " seconds." << std::endl; 
+    auto begin = std::chrono::high_resolution_clock::now(); 
+    solve();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    
+    // Print board size, number of solutions, and execution time 
+    std::cout << "Board Size: " << N << std::endl; 
+    std::cout << "Number of solutions: " << numofSol << std::endl; 
+    std::cout << "Execution time: " << elapsed.count() * 1e-9 << " seconds." << std::endl; 
 
-return 0;
+    return 0;
 }
